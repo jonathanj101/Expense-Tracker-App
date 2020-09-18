6009c190a93333484d9a1fba156d83c64c93dff8
jonathanj101 <jonatjimen@gmail.com>
2020-09-17 22:41:33 -0400

    refactored code! missing localStorage
    refactored code! missing localStorage

--- main.js
+++ main.js
@@ -1,109 +1,45 @@
-// document.addEventListener('DOMContentLoaded', function () {
-//     getFromLocalStorage()
-// })
-
-const tBody = document.querySelector(".t-body")
-const description = document.querySelector("#description")
-const amount = document.querySelector("#amount")
-const payment_type = document.querySelector("#payment-type")
-const purchaseDate = document.querySelector("#date")
-const input = document.querySelectorAll('input')
-
-const buttonSubmit = document.querySelector(".submit")
-const buttonRemove = document.querySelector('.remove')
-document.addEventListener('DOMContentLoaded', function () {
-    getFromLocalStorage()
-})
-
-let expense_detail = []
+const submitBtn = document.querySelector(".submit")
+submitBtn.addEventListener("click", addExpense)
 
 function addExpense() {
-    const tr = document.createElement('tr')
-    tr.className = "tb-tr"
-
-    const tdDesc = document.createElement("td")
-    tdDesc.textContent = description.value
-
-    const tdAmount = document.createElement("td")
-    tdAmount.textContent = `$${amount.value}`
-
-    const tdPaymentType = document.createElement("td")
-    var displayPaymentText = payment_type.options[payment_type.selectedIndex].text
-    tdPaymentType.textContent = displayPaymentText
-
-    const tdPurchaseDate = document.createElement("td")
-    const date = new Date()
-    let gettingDateFormat = `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`
-
-    const buttonRemove = document.createElement('td')
-    buttonRemove.className = 'remove'
-    buttonRemove.innerHTML = '&times;'
-
-    tdPurchaseDate.textContent = gettingDateFormat
-
-    tBody.appendChild(tr)
-
-    tr.appendChild(tdDesc)
-    tr.appendChild(tdAmount)
-    tr.appendChild(tdPaymentType)
-    tr.appendChild(tdPurchaseDate)
-    tr.appendChild(buttonRemove)
-
-    input.forEach(e => {
-        e.value = ''
-    })
-
-    expense_detail.push({
-        desc: tdDesc.textContent,
-        amount: tdAmount.textContent,
-        paymentType: tdPaymentType.textContent,
-        dateOfPurchase: tdPurchaseDate.textContent
-    })
+    const description = document.querySelector("#description").value
+    const amount = document.querySelector("#amount").value
+    const type = document.querySelector("#payment-type").value
+    const date = document.querySelector("#date").value
 
 
-    setLocalStorage(expense_detail)
+    if (description === '' || amount === '' || type === '' || date === '') {
+        alert("Empty expenses detail ==> No expenses to track!")
+    } else {
+        const expenses = [description, amount, type, date]
+        displayExpense(expenses)
+    }
+    const input = document.querySelectorAll('input')
 
-    deleteExpense(buttonRemove)
+    input.forEach(e => e.value = '')
+}
+
+function displayExpense(expenses) {
+    const tBody = document.getElementById("t-body")
+    const tRow = document.createElement('tr')
+
+    tBody.appendChild(tRow)
+
+    for (i = 0; i < expenses.length; i++) {
+        const td = document.createElement('td')
+        td.textContent = expenses[i]
+        tRow.appendChild(td)
+
+    }
+    const deleteBtn = document.createElement('td')
+    deleteBtn.classList.add('remove')
+    deleteBtn.innerHTML = "&times;"
+    tRow.appendChild(deleteBtn)
+
+    deleteExpense(deleteBtn)
+
 }
 
 function deleteExpense(buttonRemove) {
-    buttonRemove.addEventListener("click", function (e) {
-        e.target.parentElement.remove()
-        let filtered = expense_detail.filter(expenseSelected => {
-            return expenseSelected !== expense_detail[0]
-        })
-        expense_detail = filtered
-        setLocalStorage(expense_detail)
-    })
+    buttonRemove.addEventListener("click", (e) => e.target.parentElement.remove())
 }
-
-function setLocalStorage(expense_detail) {
-    localStorage.setItem('Expenses', JSON.stringify(expense_detail))
-}
-
-function getFromLocalStorage() {
-    let expense = JSON.parse(localStorage.getItem("Expenses"))
-    if (expense !== null) {
-        expenses = expense
-        expenses.filter(e => {
-            addExpense(e)
-            // working on it still
-            // console.log(typeof (e.desc))
-            // const tdDesc = document.createElement("td")
-            // tdDesc.textContent = e.desc
-
-            // const tdAmount = document.createElement("td")
-            // tdAmount.textContent = `$${e.amount}`
-
-            // const tdPaymentType = document.createElement("td")
-            // var displayPaymentText = payment_type.options[payment_type.selectedIndex].text
-            // tdPaymentType.textContent = e.paymentType
-
-            // const tdPurchaseDate = document.createElement("td")
-            // const date = new Date()
-            // let gettingDateFormat = e.dateOfPurchase
-        })
-    }
-}
-
-buttonSubmit.addEventListener("click", addExpense)
